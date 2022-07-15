22cca86ec5c9b8af5075b99596e92b7d40c9459e
Rommel Nicolás Zambrano Gaona <84996374+RommelZambrano@users.noreply.github.com>
2022-07-15 03:04:38 -0500

    elimined copied folder src

--- 06-Code/02ServiciosWeb/EuroByteWebNode_API_REST/src - Copy/index.js
+++
@@ -1,7 +0,0 @@
-import app from './app';
-import './database';
-import config from './config';
-
-app.listen(config.PORT, config.HOST, function () {
-    console.log(`Server listening on http://${config.HOST}:${config.PORT}`);
-  });
