1e841f4792290f8030c26fe40370ab9a39d480bc
Rommel Nicolás Zambrano Gaona <84996374+RommelZambrano@users.noreply.github.com>
2022-07-20 06:43:48 +0000

    format documents

--- 06-Code/03WebClient/EuroByte_React_api_front-end/eurobyte_react_api_front-end/src/services/ApiPublic-Axios.js
+++ 06-Code/03WebClient/EuroByte_React_api_front-end/eurobyte_react_api_front-end/src/services/ApiPublic-Axios.js
@@ -1,15 +1,15 @@
-import axios from 'axios'
+import axios from "axios";
 
-const baseUrl='https://api.plos.org'
+const URI = "https://api.plos.org";
 
-export async function getTitle(title){
-    try{
-        const response= await axios({
-            url:`${baseUrl}/search?q=title:${title}`,
-            method:'GET',
-        })
-        return response
-    }catch(error){
-        console.log(error)
-    }
+export async function getTitle(title) {
+  try {
+    const response = await axios({
+      url: `${URI}/search?q=title:${title}`,
+      method: "GET",
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 }
--- 06-Code/03WebClient/EuroByte_React_api_front-end/eurobyte_react_api_front-end/src/views/api_public/Api-List.js
+++ 06-Code/03WebClient/EuroByte_React_api_front-end/eurobyte_react_api_front-end/src/views/api_public/Api-List.js
@@ -1,72 +1,72 @@
-import ListApi from "../../components/api_public/Api-List"
+import ListApi from "../../components/api_public/Api-List";
 import NavBar from "../../components/Navigation";
-import { getTitle } from "../../services/ApiPublic-Axios"
-import React, { useEffect, useState } from "react"
-import Box from '@mui/material/Box';
+import { getTitle } from "../../services/ApiPublic-Axios";
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
 import Cookies from "universal-cookie/es6";
 import { makeStyles } from "@material-ui/core/styles";
 
 const cookies = new Cookies();
 
 const useStyles = makeStyles((theme) => ({
-    Box: {
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "100% 110%",
-      backgroundAttachment: "fixed",
-      backgroundColor: "#d4c4fb",
-      width: "100%",
-      height: "110%",
-      position: "absolute", 
-    },
-    h2: {
-      flexDirection: "column",
-      justifyContent: "center",
-      textAlign: "center",
-    },
-  }));
+  Box: {
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "100% 110%",
+    backgroundAttachment: "fixed",
+    backgroundColor: "#d4c4fb",
+    width: "100%",
+    height: "110%",
+    position: "absolute",
+  },
+  h2: {
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+}));
 
-const ApiList=()=>{
-    const classes = useStyles();
-    const [formPublications, setformPublications]=useState({
-        id:"github"
-    })
-    const[documents, setDocuments]=useState([])
+const ApiList = () => {
+  const classes = useStyles();
+  const [formPublications, setformPublications] = useState({
+    id: "github",
+  });
+  const [documents, setDocuments] = useState([]);
 
-    
-
-    useEffect(()=>{
-        async function loadDocuments(){
-            try {
-                const response=await getTitle(formPublications.id)
-                if(response.status===200){
-                    setDocuments(response.data)
-                }
-            } catch (error) {                
-            }            
+  useEffect(() => {
+    async function loadDocuments() {
+      try {
+        const response = await getTitle(formPublications.id);
+        if (response.status === 200) {
+          setDocuments(response.data);
         }
+      } catch (error) {}
+    }
 
-        loadDocuments()
-    },[formPublications])
+    loadDocuments();
+  }, [formPublications]);
 
-    useEffect(() => {
-        if (
-          typeof cookies.get("email") === "undefined" ||
-          cookies.get("type", { path: "/" }) !== "1"
-        ) {
-          window.location.href = "./";
-        }
-      });
-    return(
-        <Box  className={classes.Box}>
-             <NavBar />
+  useEffect(() => {
+    if (
+      typeof cookies.get("email") === "undefined" ||
+      cookies.get("type", { path: "/" }) !== "1"
+    ) {
+      window.location.href = "./";
+    }
+  });
+  return (
+    <Box className={classes.Box}>
+      <NavBar />
 
-<h2 className={classes.h2}>LISTA DE REVISTAS CIENTIFICAS</h2>
+      <h2 className={classes.h2}>LISTA DE REVISTAS CIENTIFICAS</h2>
 
-
-            <ListApi documents={documents}  formPublications={formPublications} setformPublications={setformPublications}/>
-            <br/>
-        </Box>
-    )
-}
+      <ListApi
+        documents={documents}
+        formPublications={formPublications}
+        setformPublications={setformPublications}
+      />
+      <br />
+    </Box>
+  );
+};
 
 export default ApiList;
