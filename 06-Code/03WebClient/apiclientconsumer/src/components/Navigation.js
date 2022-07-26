b68aa032cba5ceb261b9426484d2d5ed70580832
IsmaelSalazar <85363521+IsmaelSalazar@users.noreply.github.com>
2022-07-25 22:48:34 -0500

    add component

--- 06-Code/03WebClient/apiclientconsumer/src/components/Navigation.js
+++ 06-Code/03WebClient/apiclientconsumer/src/components/Navigation.js
@@ -1,1 +1,149 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import AdbIcon from "@mui/icons-material/Adb";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@mui/material/IconButton";
 
+const useStyles = makeStyles((theme) => ({
+  link: {
+    
+    textDecoration: "none",
+    color: "white",
+    fontSize: "20px",
+    marginLeft: theme.spacing(20),
+    "&:hover": {
+      color: "#F5EB19",
+      borderBottom: "0.9px solid #17202A",
+    },
+    AppBar: {
+      backgroundColor: "#17202A",
+    },
+  },
+}));
+const NavigationAdmin = () => {
+  const classes = useStyles();
+  const [anchorForms, setAnchorForms] = React.useState(null);
+  const [anchorReports, setAnchorReports] = React.useState(null);
+  const [anchorAccount, setAnchorAccount] = React.useState(null);
+  const [setAnchorAdmin] = React.useState(null);
+ 
+  const handleListJournals = () => {
+    window.location.href = "/listDocs";
+  };
+  const IconRef = () => {
+    window.location.href = "/";
+  };
+  const handleMenuAdmin = (event) => {
+    setAnchorAdmin(event.currentTarget);
+  };
+  const handleOpenReportMenu = (event) => {
+    setAnchorReports(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorAccount(null);
+    setAnchorAdmin(null);
+    setAnchorForms(null);
+    setAnchorReports(null);
+  };
+
+  return (
+    <AppBar position="static" style={{ backgroundColor: "#17202A" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <IconButton color="inherit" className={classes.link}>
+            <AdbIcon onClick={IconRef} />
+          </IconButton>
+          <box
+            sx={{ display: { xs: "none", md: "flex", marginRight: "auto" } }}
+          >
+            <Menu
+              sx={{ mt: "45px" }}
+              id="subMenuForms"
+              anchorEl={anchorForms}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorForms)}
+              onClose={handleClose}
+            >
+            </Menu>
+          </box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <Button
+              className={classes.link}
+              key="reports"
+              sx={{
+                display: {
+                  xs: "none",
+                  color: "white",
+                  md: "flex",
+                  flexGrow: 0.02,
+                },
+                mr: 4,
+              }}
+              onClick={handleOpenReportMenu}
+            >
+              Registros API
+            </Button>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="subMenuReports"
+              anchorEl={anchorReports}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              keepMounted
+              open={Boolean(anchorReports)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleListJournals}>
+                <Typography textAlign="center">API Publica</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 0.02 }}>
+            <IconButton
+              size="large"
+              className={classes.link}
+              aria-label="account of current user"
+              aria-controls="menuapp"
+              aria-haspopup="true"
+              color="inherit"
+              onClick={handleMenuAdmin}
+            >
+              {" "}
+            </IconButton>
+          </Box>
+          <Box sx={{ flexGrow: 0.02 }}>
+            <Menu
+              sx={{ mt: "0.2px" }}
+              id="menu-appbar"
+              anchorEl={anchorAccount}
+              onClose={handleClose}
+              keepMounted
+              open={Boolean(anchorAccount)}
+            >
+
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default NavigationAdmin;
